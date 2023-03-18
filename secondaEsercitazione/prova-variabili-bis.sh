ba8065f8fda5fc529135c7ca7d2b9c00bfcdbb85
letizialeonardi <letizia.leonardi@unimore.it>
2023-03-18 17:27:01 +0100

    Esercitazione del 16/3/2023

---
+++ secondaEsercitazione/prova-variabili-bis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#File prova-var-bis.sh
+echo Il valore della variabile x = $x #stampo il valore della variabile x
+echo Il valore della variabile y = $y #stampo il valore della variabile y
+echo Il valore della variabile z = $z #stampo il valore della variabile z
+z=0 #azzero il valore di z
+echo Nuovo valore della variabile z = $z #stampo il valore aggiornato della variabile z
---
+++ secondaEsercitazione/prova-variabili.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#File prova-var.sh
+echo Il valore della variabile x = $x #stampo il valore della variabile x
+echo Il valore della variabile y = $y #stampo il valore della variabile y
+echo Il valore della variabile z = $z #stampo il valore della variabile z
+
